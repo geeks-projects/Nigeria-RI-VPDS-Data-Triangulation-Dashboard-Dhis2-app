b52a539dde76a7e2eb7740ebb349e5399a7405ac
Amanyiraho <amanyiraho@gmail.com>
2021-10-12 07:51:35 +0300

    initialcommit

---
+++ Nigeria-Routine-Immunization-And-Vaccine-Preventable-Diseases-Surveillance-Data-Triangulation-Dashboard/d2.config.js
@@ -0,0 +1,10 @@
+const config = {
+    type: 'app',
+    title: 'Nigeria Routine Immunization And Vaccine Preventable Diseases Surveillance Data Triangulation Dashboard',
+
+    entryPoints: {
+        app: './src/App.js',
+    },
+}
+
+module.exports = config
---
+++ Nigeria-Routine-Immunization-And-Vaccine-Preventable-Diseases-Surveillance-Data-Triangulation-Dashboard/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DataQuery } from '@dhis2/app-runtime'
+import i18n from '@dhis2/d2-i18n'
+import classes from './App.module.css'
+
+const query = {
+    me: {
+        resource: 'me',
+    },
+}
+
+const MyApp = () => (
+    
+    <div>
+      <iframe height="800" width="100%" frameborder="no" src="https://afenetgeeks.shinyapps.io/RI-VPDs-triangulation-dashboard-development-version/"> </iframe>
+    </div>
+)
+
+export default MyApp
